7031dba2b373df86782003819603c1cb3da8b7e3
alexis.lollierou <alexis261296@gmail.com>
2026-01-05 15:52:25 +0100

    panier

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const semi = document.getElementById("typeSemi");
-  const classique = document.getElementById("typeClassique");
-  const panier = document.getElementById("lePanier");
+const classique = document.getElementById("typeClassique");
+const panier = document.getElementById("lePanier");
 
   function updatePanier() {
+    let total = 0;
+    let contenu = "";
+
     if (semi.checked) {
-      classique.checked = false;
-      panier.textContent = "Semi-marathon : 90 €";
-    } else if (classique.checked) {
-      semi.checked = false;
-      panier.textContent = "Marathon classique : 130 €";
-    } else {
+      contenu += "Semi-marathon : 90 €<br>";
+      total += 90;
+    }
+
+    if (classique.checked) {
+      contenu += "Marathon classique : 130 €<br>";
+      total += 130;
+    }
+
+    if (total === 0) {
       panier.textContent = "Aucune sélection pour le moment";
+    } else {
+      panier.innerHTML = `
+        ${contenu}
+        <strong>Total : ${total} €</strong>
+      `;
     }
   }
 
